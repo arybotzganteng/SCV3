a17a8160bbcf7c77ecf61a76c99478be3f76ddd1
Nanzz <99862432+Nanzz681@users.noreply.github.com>
2022-04-13 02:22:55 +0700

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,38 +1,19 @@
-{
+const { facebook } = require('../lib/facebook')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
+  if (!args[0].match(/https:\/\/.*(facebook.com|fb.watch)/gi)) throw `url salah`
+  facebook(args[0]).then(async res => {
+    let fb = JSON.stringify(res)
+    let json = JSON.parse(fb)
+    // m.reply(require('util').format(json))
+    if (!json.status) throw json
+    await m.reply(wait)
+    await conn.sendFile(m.chat, json.data[0].url, '', '©RadBotZ', m)
+  }).catch(_ => _)
+} 
+handler.help = ['fb'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+ 
+handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
 
-  "status": "OK",
-  "creator": "ZENZ API",
-  "result": {
-    "url": "https://fb.watch/9F1vsetkrG/",
-    "title": "Facebook Video",
-    "thumbnail": "https://scontent-ams4-1.xx.fbcdn.net/v/t15.5256-10/204281606_943689446176702_6687363109280490581_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=df419e&_nc_ohc=X0ddo7Wb7owAX-4aWgj&_nc_ht=scontent-ams4-1.xx&oh=00_AT-oOmxARjt-ZWIKV3-DpiBPizXQScQiZRZBKAxXJyQUKg&oe=625A812F",
-    "duration": null,
-    "source": "facebook",
-    "medias": [
-      {
-        "url": "https://video-ams4-1.xx.fbcdn.net/v/t42.1790-2/212070662_361166645357443_2613417099453876928_n.mp4?_nc_cat=100&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjMwMCwicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=1rzuJSKyFrgAX8XExo8&rl=300&vabr=155&_nc_ht=video-ams4-1.xx&oh=00_AT9lGoqCOYRXzAmCWTqkbgzPDT8nxiH8UnRFB_qpdxZoWg&oe=6255F51B",
-        "quality": "sd",
-        "extension": "mp4",
-        "size": 607936,
-        "formattedSize": "593.69 KB",
-        "videoAvailable": true,
-        "audioAvailable": true,
-        "chunked": false,
-        "cached": false
-      },
-      {
-        "url": "https://video-ams4-1.xx.fbcdn.net/v/t39.25447-2/277564320_393119148937922_3585591497806413539_n.mp4?_nc_cat=110&vs=be8581ad4ec0ed43&_nc_vs=HBksFQAYJEdLQkxpeERDRWpvdmltVUJBT05fYVZCNGxNSXhibWRqQUFBRhUAAsgBABUAGCRHR0kydXd3dW1sVHl4T3NDQUxqRFp6U1lsSUEwYnY0R0FBQUYVAgLIAQBLBogScHJvZ3Jlc3NpdmVfcmVjaXBlATENc3Vic2FtcGxlX2ZwcwAQdm1hZl9lbmFibGVfbnN1YgAgbWVhc3VyZV9vcmlnaW5hbF9yZXNvbHV0aW9uX3NzaW0AKGNvbXB1dGVfc3NpbV9vbmx5X2F0X29yaWdpbmFsX3Jlc29sdXRpb24AEWRpc2FibGVfcG9zdF9wdnFzABUAJQAcAAAm9JzdsveRrQMVAigCQzMYC3Z0c19wcmV2aWV3HBdAPwAAAAAAABggZGFzaF92NF81c2VjZ29wX2hxMV9mcmFnXzJfdmlkZW8SABgYdmlkZW9zLnZ0cy5jYWxsYmFjay5wcm9kOBJWSURFT19WSUVXX1JFUVVFU1QbCYgVb2VtX3RhcmdldF9lbmNvZGVfdGFnBm9lcF9oZBNvZW1fcmVxdWVzdF90aW1lX21zATAMb2VtX2NmZ19ydWxlB3VubXV0ZWQTb2VtX3JvaV9yZWFjaF9jb3VudAQ0MjI5EW9lbV9pc19leHBlcmltZW50AAxvZW1fdmlkZW9faWQPOTQzNjg5MDcyODQzNDA2Em9lbV92aWRlb19hc3NldF9pZA85NDM2ODkwNjI4NDM0MDcVb2VtX3ZpZGVvX3Jlc291cmNlX2lkDzk0MzY4OTA1OTUxMDA3NBxvZW1fc291cmNlX3ZpZGVvX2VuY29kaW5nX2lkDzM1MDM3NjA0MzczMTUwOSUCHAAlxAEbB4gBcwQxNTI4AmNkCjIwMjEtMDctMDUDcmNiBDQyMDADYXBwF0FuZHJvaWQgxLDDp2luIEZhY2Vib29rAmN0GUNPTlRBSU5FRF9QT1NUX0FUVEFDSE1FTlQTb3JpZ2luYWxfZHVyYXRpb25fcwYzMS4xMTUCdHMVcHJvZ3Jlc3NpdmVfZW5jb2RpbmdzAA%3D%3D&ccb=1-5&_nc_sid=894f7d&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCJ9&_nc_ohc=t3M8v72OD54AX_9Szax&_nc_ht=video-ams4-1.xx&oh=00_AT9qDPYUeaW0kea2LkHsDpJT0tYllcVYcbJi8MzH9hn-9w&oe=6259C5C5&_nc_rid=505374590564913",
-        "quality": "hd",
-        "extension": "mp4",
-        "size": 737632,
-        "formattedSize": "720.34 KB",
-        "videoAvailable": true,
-        "audioAvailable": true,
-        "chunked": false,
-        "cached": false
-      }
-    }
-
-  }
-
-}
+module.exports = handler
