568de3c265cb60a95e8ca0577cbc3e7fea9014ef
Nanzz <99862432+Nanzz681@users.noreply.github.com>
2022-04-14 16:03:43 +0700

    Update yvebot.js

--- plugins/yvebot.js
+++ plugins/yvebot.js
@@ -3,7 +3,7 @@ let PhoneNumber = require('awesome-phonenumber')
 let levelling = require('../lib/levelling')
   
 let handler = async (m, { conn }) => {
-let caption = `*HALO Bang*\n*Saya RadBotZ*\n*Pilih Menu Di Bawah Yaa Bree*\n*Spam = Banned*\n*Telp = Blok*\n\n*Join Group WhatsApp*\nhttps://chat.whatsapp.com/LM77ornJmDz2tMIkEIYKGe\n\n*Untuk Melihat Menu Bot Ketik .menu*`.trim()
+let caption = `*HALO Bang*\n*Saya NanzzBotz*\n*Pilih Menu Di Bawah Yaa Bree*\n*Spam = Banned*\n*Telp = Blok*\n\n*Join Group WhatsApp*\nhttps://chat.whatsapp.com/EthkDwgA3cT2TPGJmHbjgT\n\n*Untuk Melihat Menu Bot Ketik .menu*`.trim()
 conn.send3Button(m.chat, caption, `Powered By Baileys\nCreated RadBotZ By Raditya\n`.trim(), '📑MENU', '.?', '🍭Owner', '.owner', '📖Info Bot', '.infobot', m)
 }
 
