f045d42c5b611b49b3d22f255d622da78e5f0316
AryBotz <101872515+arybotzganteng@users.noreply.github.com>
2022-04-15 15:18:36 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,20 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(bank)).buffer(), `
-*────────「 DONATE 」 ────────*
-
-Hai 👋
-Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
-┌〔 Donasi • Emoney 〕
-├ PULSA : 6285730903853
-├ DANA : 6285730903853
-└────
-`.trim(), 'Donasi bg Biar Bot Nya on 24Jam', 'SEWA', '.sewa')
+*───────「 DONATE 」 ─────*
+𝙃𝙖𝙣𝙮𝙖 𝙢𝙚𝙣𝙚𝙧𝙞𝙢𝙖 𝙢𝙖𝙩𝙖 𝙪𝙖𝙣𝙜 𝙢𝙖𝙡𝙖𝙮𝙨𝙞𝙖
+╭═══════════════
+║╭─── [ DONASI ] ───
+║│➸ _*Hotlink*_✅ : 
+║│- 60177945983
+║│
+║│➸ _*Maxis*_✅: 
+║│- 60147751029
+║│
+║│➸ _*OWNER*_
+║│- https://wa.me/79519874382?text=.menu
+║╰────────────
+╰═══════════════
+`.trim(), 'Donasi jan asal mencet', 'Owner Bot👤', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
