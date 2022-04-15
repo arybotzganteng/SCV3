e8c87cf55be3f6b3af61bc145a9f1c97acbbf03e
AryBotz <101872515+arybotzganteng@users.noreply.github.com>
2022-04-16 01:09:46 +0800

    Update biodata.js

--- plugins/biodata.js
+++ plugins/biodata.js
@@ -3,9 +3,10 @@ let handler = async (m, { conn, text }) => {
   let ext= `
 *BIODATA OWNER*
 
-*Nama* : Raditya
-*Umur* : 19
-*Kelas* : 3 Smk
+*Nama* : NoelDaniel
+*Umur* : 14
+*Agama* : Kristen
+*Kelas* : Private
 *Status* : Private
 
 *SOSIAL MEDIA*
@@ -13,8 +14,8 @@ let handler = async (m, { conn, text }) => {
 *instagran* : Private
 *Facebook* : Private
 *Chanel Youtube* : none
-*Gmail* : Xnxx@gmail.com
-*Github* : https://github.com/Drz103
+*Gmail* : noeldaniel400@gmail.com
+*Github* : https://github.com/arybotzganteng
 
 _Oke udah itu aja terimakasih_
 
@@ -22,9 +23,9 @@ _Oke udah itu aja terimakasih_
 conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By Raditya', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
 
 }
-handler.help = ['infoowner', 'infocreator']
+handler.help = ['infoary', 'infocreator']
 handler.tags = ['info']
-handler.command = /^(infoowner|infocreator)$/i
+handler.command = /^(infoary|infocreator)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
