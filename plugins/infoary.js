24377907fc520a17f2fa4cbdfc2f07b862dbd055
AryBotz <101872515+arybotzganteng@users.noreply.github.com>
2022-04-15 15:17:01 +0800

    Create infoary.js

---
+++ plugins/infoary.js
@@ -0,0 +1,13 @@
+╭─˗ˏˋ *_INFORMASI OWNER_* ´ˎ˗
+│ ✎ _Nama_ : *NoelDaniel*
+│ ✎ _Umur_ : *14*
+│ ✎ _Kelas_ : *privasi*
+│ ✎ _Asal_ : *SABAH,MALAYSIA*
+│ ✎ Jantina : *Lelaki*
+│ ✎ Status : *Males Pacaran*
+│ ✎ _Official Grup_ : *https://chat.whatsapp.com/BCgUSCxQltq8SwcpJe1tDf*
+│ ✎ _TikTok_ : *https://vt.tiktok.com/ZSdRaWNh1/*
+│ ✎ Github : *http://github.com/arybotzganteng*
+│ ✎ _WhatsApp_ :
+│    *https://wa.me/79519874382*
+╰‿‿‿‿‿‿‿‿
