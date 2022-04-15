86d96bac771b97a3af89b10f692d3522990c8461
AryBotz <101872515+arybotzganteng@users.noreply.github.com>
2022-04-15 15:36:56 +0800

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,21 +1,22 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *2 minggu* :      *Rp 7000*
-╠➥ *1 Bulan* :      *Rp 10000*
-╠➥ *Permanen* : *Rp 50000*
-╠➥ *Owner* :   *Rp 30000*/bln
-╠➥ *Nomer Gopay & pulsa* : 
-║    085730903853
+╠➥ *2 minggu* :      *RM5.00*
+╠➥ *1 Bulan* :      *RM10.00*
+╠➥ *Permanen* : *RM30*
+╠➥ *Owner* :   *RM20*/bln
+╠➥ *Nomer Topup Maxis/Hotlink* : 
+║    60147751029
 ╠═〘 PEMBAYARAN 〙 ═
-╠➥ Dana Dan Pulsa
-║
+╠➥ *Topup Maxis √*
+║➥ *Topup Hotlink √*
+╠
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Ketuk Tombol Di Bawah Ya
 ║
 ╠═ ©2021 Rpg wabot-aq
 ╠═ Scrip original by Nurutomo
-╠═〘 AlyaaXzy 〙 ═`.trim(), watermark, 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
+╠═〘 AryBotz 〙 ═`.trim(), watermark, 'Maxis', '#viaHotlink', 'Topup ', '#viaKredit', m)
 }
 
 handler.command = /^sewabot$/i
